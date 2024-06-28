19d40985b1aa0300dad3d355681006b751a549d4
Juanliendo22 <liendojuan070@gmail.com>
2024-06-28 17:14:35 -0300

    Test 1

--- backend-cng/app/cloudinary/cloudinaryConfig.py
+++
@@ -1,9 +0,0 @@
-import cloudinary
-from config import settings
-
-cloudinary.config( 
-    cloud_name = settings.cloud_name_cloudinary,
-    api_key = settings.api_key_cloudinary,
-    api_secret = settings.api_secret_cloudinary,
-    secure=True
-)
--- frontend-cng/src/components/pages/routeNotFound/RouteNotFound.jsx
+++
@@ -1,9 +0,0 @@
-import swal from "sweetalert";
-
-const RouteNotFound = () => {
-    return swal("404 ROUTE NOT FOUND", {
-        className: "red-bg",
-    });
-};
-
-export default RouteNotFound;
