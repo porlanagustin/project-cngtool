073a9707a77bb816854b9af304490849bc8323de
Juanliendo22 <liendojuan070@gmail.com>
2024-07-12 17:14:14 -0300

    cambios en el home

--- frontend-cng/src/components/pages/homePage/Homepage.jsx
+++ frontend-cng/src/components/pages/homePage/Homepage.jsx
@@ -1,5 +1,28 @@
 const Homepage = () => {
-    <div>Hola</div>
-}
+  return (
+    <div className="homepage">
+      {/* Bienvenida y Resumen */}
+      <section className="welcome">
+        <h1>Bienvenido al Concierge Tool</h1>
+        <p>
+          Una herramienta para ayudarte a explorar lugares para visitar, teatros
+          y restaurantes.
+        </p>
+      </section>
+
+      {/* Información de Contacto */}
+      <section className="contact-info">
+        <h2>¿Necesitas Ayuda?</h2>
+        <p>
+          Para soporte, contacta a{" "}
+          <a href="mailto:support@conciergetool.com">
+            support@conciergetool.com
+          </a>
+          .
+        </p>
+      </section>
+    </div>
+  );
+};
 
 export default Homepage;
